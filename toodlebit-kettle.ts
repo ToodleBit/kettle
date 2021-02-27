8adf9cf6aa9c0712f0015cf63987b634e37df4ba
ToodleBit <45330091+ToodleBit@users.noreply.github.com>
2021-02-27 22:37:15 +0000

    Update toodlebit-kettle.ts

--- toodlebit-kettle.ts
+++ toodlebit-kettle.ts
@@ -1,7 +1,16 @@
-/**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
- */
+
+let heatercondition = false
+let watertemp = 50
+let setpin = DigitalPin.P0
+
+enum MyColours {
+	//% block="Green"
+	Green,
+	//% block="Red"
+	Red,
+	//% block="Amber"
+	Amber
+}
 
 enum MyOptions {
     //% block="on"
@@ -10,140 +19,121 @@ enum MyOptions {
     Off
 }
 
-enum MyScreenOptions {
-    //% block="yes"
-    Yes,
-    //% block="no"
-    No
+enum MyPinOptions {
+    //% block="Pin0"
+    Zero,
+    //% block="Pin1"
+    One,
+    //% block="Pin2"
+    Two
 }
+
+enum MyHeating {
+    //% block="on"
+    on,
+    //% block="off"
+    off
+}
+
+
 /**
  * Custom blocks
  */
 //% color=#008C8C weight=10 icon="\uf0eb"
-namespace ToodleLights {
+namespace Kettle {
 
-
-    let setpin = DigitalPin.P0
-    let screenoutput = 1
-
-    /**
-    * Choose red light on or off
-    * @param pin describe parameter here, eg: AnalogPin.P0
-    * @param myoption either on or off, eg: on, off
-    */
-    //% weight=10
-    //% blockId=toodlebit_red block="red light %pin| %MyOptions"
-    export function set_redlight(pin: AnalogPin, myoption: MyOptions): void {
-        // Add code here
-
-        switch (pin) {
-            case AnalogPin.P0:
-                setpin = DigitalPin.P0
-                break
-            case AnalogPin.P1:
-                setpin = DigitalPin.P1
-                break
-            case AnalogPin.P2:
-                setpin = DigitalPin.P2
-                break
-            default:
-                setpin = DigitalPin.P0
-        }
-
-        if (myoption == 0) {
-
-            pins.digitalWritePin(setpin, 1)
-            if (screenoutput == 1) { led.plot(2, 0) }
-        } else {
-            pins.digitalWritePin(setpin, 0)
-            if (screenoutput == 1) { led.unplot(2, 0) }
-        }
-
-
+/**
+* This is a reporter block that returns a number
+*/
+//%  block="Water Temperature"
+    export function WaterTemp(): number {
+    return watertemp;
     }
-
-    /**
+	
+/**
 * Choose amber light on or off
 * @param pin describe parameter here, eg: AnalogPin.P0
-* @param cond either on or off, eg: on, off
+* @param myoption either on or off, eg: on, off
 */
     //% weight=10
-    //% blockId=toodlebit_amber block="amber light %pin| %cond"
-    export function set_amberlight(pin: AnalogPin, myoption: MyOptions): void {
+    //% blockId=toodlebit_amber block="Turn %LColour light %myoption select %pin"
+    export function set_light(LColour: MyColours, myoption: MyOptions, pin: MyPinOptions): void {
         // Add code here
-
         switch (pin) {
-            case AnalogPin.P0:
+            case 0:
                 setpin = DigitalPin.P0
                 break
-            case AnalogPin.P1:
+            case 1:
                 setpin = DigitalPin.P1
                 break
-            case AnalogPin.P2:
+            case 2:
                 setpin = DigitalPin.P2
                 break
             default:
                 setpin = DigitalPin.P0
-        }
+                        }
+		 if (myoption == 0) { pins.digitalWritePin(setpin, 1) } 
+         else { pins.digitalWritePin(setpin, 0) }
+		
+		}
 
-        if (myoption == 0) {
-
-            pins.digitalWritePin(setpin, 1)
-            if (screenoutput == 1) { led.plot(2, 2) }
-
-        } else {
-            pins.digitalWritePin(setpin, 0)
-            if (screenoutput == 1) { led.unplot(2, 2) }
-        }
-
-
+//% block="Turn heating element $myheating "
+    export function heatingelement(myheating: MyHeating) {
+        if (myheating == 0) {  heatercondition = true }
+        else { heatercondition = false }
+        RunHeater()
     }
 
-    /**
-    * Choose green light on or off
-    * @param pin describe parameter here, eg: AnalogPin.P0
-    * @param cond either on or off, eg: on, off
-    */
-    //% weight=10
-    //% blockId=toodlebit_green		block="green light %pin| %cond"
-    export function set_greenlight(pin: AnalogPin, myoption: MyOptions): void {
-        // Add code here
-
-        switch (pin) {
-            case AnalogPin.P0:
-                setpin = DigitalPin.P0
-                break
-            case AnalogPin.P1:
-                setpin = DigitalPin.P1
-                break
-            case AnalogPin.P2:
-                setpin = DigitalPin.P2
-                break
-            default:
-                setpin = DigitalPin.P0
+function RunHeater(){
+	while (heatercondition == true) {
+    watertemp += 1
+    
+    if (watertemp == 50){
+       led.plot(0, 4) 
+       led.plot(1, 4) 
+       led.plot(2, 4) 
+       led.plot(3, 4) 
+       led.plot(4, 4) 
+    }  
+    if (watertemp == 66){
+        led.plot(0, 3) 
+       led.plot(1, 3) 
+       led.plot(2, 3) 
+       led.plot(3, 3) 
+       led.plot(4, 3) 
         }
-
-        if (myoption == 0) {
-
-            pins.digitalWritePin(setpin, 1)
-            if (screenoutput == 1) { led.plot(2, 4) }
-
-        } else {
-            pins.digitalWritePin(setpin, 0)
-            if (screenoutput == 1) { led.unplot(2, 4) }
+        if (watertemp == 80){
+        led.plot(0, 2) 
+       led.plot(1, 2) 
+       led.plot(2, 2) 
+       led.plot(3, 2) 
+       led.plot(4, 2) 
         }
-
-
+      if (watertemp == 90){
+        led.plot(0, 1) 
+       led.plot(1, 1) 
+       led.plot(2, 1) 
+       led.plot(3, 1) 
+       led.plot(4, 1) 
+        }
+          if (watertemp == 100){
+        led.plot(0, 0) 
+       led.plot(1, 0) 
+       led.plot(2, 0) 
+       led.plot(3, 0) 
+       led.plot(4, 0) 
+        }
+        if (watertemp > 100){
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showIcon(IconNames.Chessboard)
+        
+          }
+          basic.pause(500)
     }
+    basic.clearScreen()
+    watertemp = 0
 
-    /**
-* Show traffic light output on MicroBit screen
-* @param cond either yes or no, eg: yes, no
-*/
-    //% weight=9
-    //% blockId=toodlebit_screen	block="Show on Microbit screen %cond"
-    export function set_screenoptions(myscreen: MyScreenOptions): void {
-        // Add code here
-        if (myscreen == 1) { screenoutput = 0 } else { screenoutput = 1 }
-    }
-} 
+		}
+  
+}
