6b5337f7c4dc4ea9e88a50eff0070c5913671850
ToodleBit <45330091+ToodleBit@users.noreply.github.com>
2021-02-28 19:51:03 +0000

    Update toodlebit-kettle.ts

--- toodlebit-kettle.ts
+++ toodlebit-kettle.ts
@@ -299,7 +299,7 @@ function RunHeater(){
                 for (let i = 0; i < n; ++i) {
                     if (i <= v) {
                         const b = Math.idiv(i * 255, n1);
-                        this.setPixelColor(i, neopixel.rgb(b, 0, 255 - b));
+                        this.setPixelColor(i, kettle.rgb(b, 0, 255 - b));
                     }
                     else this.setPixelColor(i, 0);
                 }
